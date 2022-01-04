7ecc56f234306963279fc79208817596dcbd7e92
Michał Szczygieł <1153719+mszczygiel@users.noreply.github.com>
2022-01-04 09:26:43 +0100

    move resources to a single vpc

--- terraform/apps.tf
+++ terraform/apps.tf
@@ -1,90 +1,12 @@
-resource "aws_vpc" "apps" {
-  cidr_block = "192.168.0.0/16"
-}
-
-# resource "aws_s3_bucket" "apps_flow_logs" {
-#     bucket = "mszczygiel-demo-apps-flow-logs"
-#     acl = "log-delivery-write"
-#     force_destroy = true
-# }
-
-# resource "aws_flow_log" "apps" {
-#   log_destination      = aws_s3_bucket.apps_flow_logs.arn
-#   log_destination_type = "s3"
-#   traffic_type         = "ALL"
-#   vpc_id               = aws_vpc.apps.id
-# }
-
-resource "aws_internet_gateway" "apps_igw" {
-  vpc_id = aws_vpc.apps.id
-}
-
-resource "aws_subnet" "apps_a" {
-  vpc_id            = aws_vpc.apps.id
-  cidr_block        = "192.168.1.0/24"
-  availability_zone = local.az
-}
-
-resource "aws_subnet" "endpoint" {
-  vpc_id            = aws_vpc.apps.id
-  cidr_block        = "192.168.10.0/24"
-  availability_zone = local.az
-}
-
-
-
-resource "aws_route_table" "apps_a" {
-  vpc_id = aws_vpc.apps.id
-
-  route {
-    cidr_block      = "0.0.0.0/0"
-    vpc_endpoint_id = aws_vpc_endpoint.censored_communication.id
-  }
-}
-
-resource "aws_route_table_association" "apps_a" {
-  subnet_id      = aws_subnet.apps_a.id
-  route_table_id = aws_route_table.apps_a.id
-}
-
-resource "aws_route_table" "igw" {
-  vpc_id = aws_vpc.apps.id
-
-  route {
-    cidr_block      = aws_subnet.apps_a.cidr_block
-    vpc_endpoint_id = aws_vpc_endpoint.censored_communication.id
-  }
-}
-
-resource "aws_route_table_association" "igw" {
-  gateway_id     = aws_internet_gateway.apps_igw.id
-  route_table_id = aws_route_table.igw.id
-}
-
-
-resource "aws_route_table" "endpoint" {
-  vpc_id = aws_vpc.apps.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.apps_igw.id
-  }
-}
-
-resource "aws_route_table_association" "endpoint" {
-  subnet_id      = aws_subnet.endpoint.id
-  route_table_id = aws_route_table.endpoint.id
-}
-
 resource "aws_vpc_endpoint" "censored_communication" {
   service_name      = aws_vpc_endpoint_service.censor_service.service_name
   subnet_ids        = [aws_subnet.endpoint.id]
   vpc_endpoint_type = aws_vpc_endpoint_service.censor_service.service_type
-  vpc_id            = aws_vpc.apps.id
+  vpc_id            = aws_vpc.main.id
 }
 
 resource "aws_security_group" "apps_allow_ssh" {
-  vpc_id = aws_vpc.apps.id
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 22
     to_port     = 22
@@ -93,18 +15,18 @@ resource "aws_security_group" "apps_allow_ssh" {
   }
 }
 resource "aws_security_group" "apps_allow_chat" {
-  vpc_id = aws_vpc.apps.id
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = local.chat_port
     to_port     = local.chat_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_subnet.apps_a.cidr_block, aws_subnet.apps_b.cidr_block]
   }
 }
 
 resource "aws_security_group" "apps_permissive_egress" {
-  vpc_id = aws_vpc.apps.id
+  vpc_id = aws_vpc.main.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -121,5 +43,16 @@ resource "aws_instance" "app_a" {
   availability_zone           = local.az
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.apps_a.id
+  user_data                   = local.init_intance
+}
+
+resource "aws_instance" "app_b" {
+  ami                         = "ami-00051469f31042765"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.default.key_name
+  vpc_security_group_ids      = [aws_security_group.apps_allow_ssh.id, aws_security_group.apps_permissive_egress.id, aws_security_group.apps_allow_chat.id]
+  availability_zone           = local.az
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.apps_b.id
   user_data                   = local.init_intance
 }
--- terraform/censor.tf
+++ terraform/censor.tf
@@ -1,71 +1,15 @@
-resource "aws_vpc" "censor" {
-  cidr_block = "192.168.0.0/16"
-}
-
-resource "aws_internet_gateway" "censor_igw" {
-  vpc_id = aws_vpc.censor.id
-}
-
-# resource "aws_s3_bucket" "censor_flow_logs" {
-#     bucket = "mszczygiel-demo-censor-flow-logs"
-#     acl = "log-delivery-write"
-#     force_destroy = true
-# }
-
-# resource "aws_flow_log" "censor" {
-#   log_destination      = aws_s3_bucket.censor_flow_logs.arn
-#   log_destination_type = "s3"
-#   traffic_type         = "ALL"
-#   vpc_id               = aws_vpc.censor.id
-# }
-
-resource aws_route_table_association "censor_igw" {
-    gateway_id = aws_internet_gateway.censor_igw.id
-    route_table_id = aws_vpc.censor.default_route_table_id
-}
-
-resource "aws_subnet" "censor" {
-  vpc_id            = aws_vpc.censor.id
-  cidr_block        = "192.168.20.0/24"
-  availability_zone = local.az
-}
-
-resource "aws_subnet" "lb" {
-  vpc_id            = aws_vpc.censor.id
-  cidr_block        = "192.168.30.0/24"
-  availability_zone = local.az
-}
-
-resource "aws_route_table" "censor" {
-    vpc_id = aws_vpc.censor.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.censor_igw.id
-    }
-}
-
-resource "aws_route_table_association" "censor" {
-  subnet_id      = aws_subnet.censor.id
-  route_table_id = aws_route_table.censor.id
-}
-
-resource "aws_route_table_association" "lb" {
-  subnet_id = aws_subnet.lb.id
-  route_table_id = aws_vpc.censor.default_route_table_id
-}
-
 resource "aws_security_group" "censor_allow_geneve" {
-  vpc_id = aws_vpc.censor.id
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = local.geneve_port
     to_port     = local.geneve_port
     protocol    = "udp"
-    cidr_blocks = [aws_subnet.lb.cidr_block]
+    cidr_blocks = [aws_subnet.appliances.cidr_block]
   }
 }
 
 resource "aws_security_group" "censor_allow_ssh" {
-  vpc_id = aws_vpc.censor.id
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 22
     to_port     = 22
@@ -75,17 +19,17 @@ resource "aws_security_group" "censor_allow_ssh" {
 }
 
 resource "aws_security_group" "censor_allow_health_check" {
-  vpc_id = aws_vpc.censor.id
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.lb.cidr_block]
+    cidr_blocks = [aws_subnet.appliances.cidr_block]
   }
 }
 
 resource "aws_security_group" "censor_permissive_egress" {
-  vpc_id = aws_vpc.censor.id
+  vpc_id = aws_vpc.main.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -103,26 +47,26 @@ resource "aws_instance" "appliance" {
   vpc_security_group_ids      = [aws_security_group.censor_allow_ssh.id, aws_security_group.censor_allow_geneve.id, aws_security_group.censor_permissive_egress.id, aws_security_group.censor_allow_health_check.id]
   availability_zone           = local.az
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.censor.id
+  subnet_id                   = aws_subnet.appliances.id
   user_data                   = local.init_intance
 }
 
 
 resource "aws_lb" "gateway" {
   load_balancer_type = "gateway"
-  subnets            = [aws_subnet.lb.id]
+  subnets            = [aws_subnet.appliances.id]
 }
 
 
 resource "aws_lb_target_group" "appliances" {
   port     = local.geneve_port
   protocol = "GENEVE"
-  vpc_id   = aws_vpc.censor.id
+  vpc_id   = aws_vpc.main.id
   health_check {
-      enabled = true
-      protocol = "TCP"
-      port = 8080
-      interval = 10
+    enabled  = true
+    protocol = "TCP"
+    port     = 8080
+    interval = 10
   }
 }
 

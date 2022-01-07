c2d89443d7d2674fd7173ddcd3923554a979dbe9
Michał Szczygieł <1153719+mszczygiel@users.noreply.github.com>
2022-01-07 09:01:43 +0100

    tune deployment scripts and add initialization script

--- deploy_censor.sh
+++ deploy_censor.sh
@@ -3,4 +3,7 @@
 source ./IPS.sh
 
 (cd censor && CGO_ENABLED=0 go build)
-scp -o StrictHostKeyChecking=no ./censor/censor ec2-user@$APPLIANCE_IP:/home/ec2-user/censor
+
+ssh -o StrictHostKeyChecking=no ec2-user@$APPLIANCE_IP sh -c '"sudo pkill censor"' || true
+scp  ./censor/censor ec2-user@$APPLIANCE_IP:/home/ec2-user/censor
+ssh ec2-user@$APPLIANCE_IP sh -c '"sudo ./censor"'
